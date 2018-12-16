1da2bf1802b3793745c7f89451f6e08775f9a326
Eri Osawa <osawaeri@users.noreply.github.com>
2018-12-16 22:59:57 +0900

    9

--- js/responsive.js
+++ js/responsive.js
@@ -2,9 +2,11 @@ $(function () {
     var ua = navigator.userAgent;
     if (ua.indexOf('iPhone') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0) {
         $('#mouse-wrapper').css('display', 'none');
+        $('#top-wrapper').css('top', '130px');
     } else if (ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0) {
         $('#mouse-wrapper').css('display', 'none');
+        $('#top-wrapper').css('top', '130px');
     } else {
         // PC用コード
     }
-})
+});
