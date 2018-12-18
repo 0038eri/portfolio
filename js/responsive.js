3789b1ddcaec6b556bdef8a9fd49bc47020b1502
Eri Osawa <osawaeri@users.noreply.github.com>
2018-12-18 17:48:57 +0900

    12

--- js/responsive.js
+++ js/responsive.js
@@ -4,17 +4,17 @@ $(function () {
     
     if (ua.indexOf('iPhone') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0) {
         
+        $('#script').attr('src','js/mediaart.js');
         $('#mouse-wrapper').css('display', 'none');
         $('#top-wrapper').css('top', '130px');
         
     } else if (ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0) {
         
+        $('#script').attr('src','js/mediaart.js');
         $('#mouse-wrapper').css('display', 'none');
         $('#top-wrapper').css('top', '130px');
         
     } else {
-        
-        $('script').attr('src','js/mediaart-pc.js');
         
     }
 });
